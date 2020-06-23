ad3efe9ac20738fdb0245a26e468dbc47129c008
leannelb <leannelaceybyrne@outlook.co>
2020-06-23 21:32:21 +0100

    adding a router module and path for heroes

--- angular-IO/tour-of-heroes/src/app/services/message.service.ts
+++ angular-IO/tour-of-heroes/src/app/services/message.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
+// The @Injectable() decorator accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
+// Injectable is what allows you to inject a service for use elsewhere
 export class MessageService {
   messages: string[] = [];
 
@@ -13,4 +15,5 @@ export class MessageService {
   clear() {
     this.messages = [];
   }
+  // The service exposes its cache of messages and two methods: one to add() a message to the cache and another to clear() the cache.
 }
